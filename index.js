83e6be6e3b2f83e8f9cfded8475cff2c41f5cbc5
SametCimen1 <cimensamet338@gmail.com>
2022-10-26 23:16:34 -0400

    pleaze work

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const cookieParser = require('cookie-parser')
 const cors = require('cors');
 //routes
 
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+
 
 
 const corsOptions ={
